ff229c702059d3335555a970e1fac118198d92c8
Alfred Githinji <alfredgithinji87@gmail.com>
2023-07-13 08:57:05 +0300

    Chabged url to live one

--- src/axios.js
+++ src/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const accessToken = localStorage.getItem("accessToken") || "";
 
 const instance = axios.create({
-  // baseURL: "https://us-central1-elearning-module-a887d.cloudfunctions.net/app",
+  baseURL: "https://us-central1-elearning-module-a887d.cloudfunctions.net/app",
   // baseURL: "http://localhost:5000/elearning-module-a887d/us-central1/app",
-  baseURL: "http://localhost:4000",
+  // baseURL: "http://localhost:4000",
   headers: {
     common: accessToken ? { Authorization: `Bearer ${accessToken}` } : {},
   },
