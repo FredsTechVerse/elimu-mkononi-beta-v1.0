a8ea2649e6c0bdd5821fd7d0d073b3ffbd28ceba
Alfred Githinji <alfredgithinji87@gmail.com>
2023-09-05 22:55:11 +0300

    User card revamp

--- src/components/Cards/UserCard.jsx
+++ src/components/Cards/UserCard.jsx
@@ -12,16 +12,27 @@ const UserCard = ({ user, userKey, role }) => {
   return (
     <div
       className={` ${
-        userKey === "odd" ? "bg-blue-200" : ""
-      }  relative flex laptop:hidden flex-col justify-evenly items-center w-[280px]  rounded-xl my-0.5 shadow-md shadow-slate-300 bg-slate-100  px-3`}
+        userKey === "odd" && "bg-blue-200"
+      }  relative flex laptop:hidden flex-col justify-evenly items-center w-[280px]  rounded-xl my-0.5 pt-5 shadow-md shadow-slate-300 bg-slate-100  px-3`}
     >
       <StatusPill status={user.status} />
-      <div className="flex items-center justify-start w-full text-lg h-20 ">
-        <p className="capitalize w-full">
-          <span>{user.firstName}</span> <span>{user.surname}</span>
-        </p>
-        {role === "EM-202" && <span>Units : {numberOfUnits(role)}</span>}
-        <CTAButton userID={user._id} contact={user.contact} role={role} />
+      <div className="flex flex-col items-center justify-between w-full text-lg h-16 py-5 ">
+        <span>
+          {role === "EM-202" && (
+            <p className="absolute top-1 left-2 flex-row-centered gap-2">
+              <div className="bg-slate-800 flex-row-centered w-9 h-5 rounded-full text-white text-sm">
+                {numberOfUnits(role)}
+              </div>
+              Units
+            </p>
+          )}
+        </span>
+        <div className="w-full flex items-center justify-between">
+          <p className="capitalize  flex gap-2 font-bold">
+            {user.firstName} {user.surname}
+          </p>
+          <CTAButton userID={user._id} contact={user.contact} role={role} />
+        </div>
       </div>
     </div>
   );
